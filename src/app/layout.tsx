1ac75586e9d314326e29c6c588515bbcc7a5564a
chronosgit <warfacemeister@mail.ru>
2024-07-04 11:42:03 -0400

    feat: updated root layout and page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,40 +2,43 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from '@/components/header/Header';
 import './globals.css';
+import { ThemeContextProvider } from '@/contexts/theme/ThemeContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const metadata: Metadata = {
-  title: 'Typewriter',
-  description:
-    'Web-application for assisting in increasing typing speed and accurasy',
-  authors: {
-    name: 'chronosgit',
-    url: 'https://github.com/chronosgit',
-  },
-  keywords: [
-    'typing',
-    'typing speed',
-    'speed',
-    'typing accuracy',
-    'typewriter',
-  ],
+	title: 'Typewriter',
+	description:
+		'Web-application for assisting in increasing typing speed and accurasy',
+	authors: {
+		name: 'chronosgit',
+		url: 'https://github.com/chronosgit',
+	},
+	keywords: [
+		'typing',
+		'typing speed',
+		'speed',
+		'typing accuracy',
+		'typewriter',
+	],
 };
 
 const RootLayout = ({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) => {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeContextProvider>
+					<Header />
 
-        {children}
-      </body>
-    </html>
-  );
+					{children}
+				</ThemeContextProvider>
+			</body>
+		</html>
+	);
 };
 
 export { metadata };
