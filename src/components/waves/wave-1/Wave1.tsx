d98dd8fe25a9d662ba21945cdd18301063c718c8
chronosgit <warfacemeister@mail.ru>
2024-07-04 18:05:04 -0400

    style: prettied

--- src/components/waves/wave-1/Wave1.tsx
+++ src/components/waves/wave-1/Wave1.tsx
@@ -1,6 +1,5 @@
 // Parent element must be relative
 const Wave1 = () => {
-
 	return (
 		<div className="absolute top-0 left-0 w-full overflow-hidden leading-none">
 			<svg
@@ -9,7 +8,7 @@ const Wave1 = () => {
 				viewBox="0 0 1200 120"
 				preserveAspectRatio="none"
 				className="relative block h-3"
-				style={{width: 'calc(100% + 1.3px)'}}
+				style={{ width: 'calc(100% + 1.3px)' }}
 			>
 				<path
 					d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
