45edbac6b1383ccc76c8aebd06cd2f1b55a651c3
chronosgit <warfacemeister@mail.ru>
2024-07-04 15:23:52 -0400

    feat: updated footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,16 +9,17 @@ const Footer = () => {
 				</p>
 
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quo,
-					eaque quae dolorum quaerat quas aspernatur perspiciatis illum tempora
-					molestiae laborum temporibus est repellendus saepe quisquam nobis totam!
-					Commodi, nobis. Doloremque reiciendis officia repellendus aliquid a aut?
-					Reprehenderit asperiores ipsum maiores consectetur iure earum voluptates
-					in, laboriosam tempora enim doloremque veritatis ullam dolores culpa
-					temporibus sequi voluptate eaque ad sit. Culpa laboriosam sed quibusdam
-					consequatur aperiam ut? Itaque animi facere sint eius ex! Quisquam
-					laboriosam voluptatum officia ipsa eos cum quasi. Quo voluptates cum
-					ipsa rem deserunt inventore vitae perspiciatis!
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+					quo, eaque quae dolorum quaerat quas aspernatur perspiciatis illum
+					tempora molestiae laborum temporibus est repellendus saepe quisquam
+					nobis totam! Commodi, nobis. Doloremque reiciendis officia repellendus
+					aliquid a aut? Reprehenderit asperiores ipsum maiores consectetur iure
+					earum voluptates in, laboriosam tempora enim doloremque veritatis
+					ullam dolores culpa temporibus sequi voluptate eaque ad sit. Culpa
+					laboriosam sed quibusdam consequatur aperiam ut? Itaque animi facere
+					sint eius ex! Quisquam laboriosam voluptatum officia ipsa eos cum
+					quasi. Quo voluptates cum ipsa rem deserunt inventore vitae
+					perspiciatis!
 				</p>
 			</div>
 		</footer>
