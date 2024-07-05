53613744c899684572a07796d9e95503c7547f5e
chronosgit <warfacemeister@mail.ru>
2024-07-05 07:10:02 -0400

    feat: updated tech-stack section

--- src/app/_components/tech-stack/TechStack.tsx
+++ src/app/_components/tech-stack/TechStack.tsx
@@ -3,8 +3,14 @@ import techStack from "@/app/_utils/techStack";
 const TechStack = () => {
 
 	return (
-		<section className="">
-			<div className="w-full flex items-center gap-4">
+		<section className="py-8 px-2">
+			<h2 className="text-3xl font-bold">
+				<span className="text-red-500">Typewriter </span>
+				is <em>universal</em> service
+			</h2>
+
+			{/* Change below to infinite gallery */}
+			<div className="p-8 w-full flex items-center gap-32">
 				{
 					techStack.map(t => <div className="scale-400">{t.icon}</div>)
 				}
--- src/app/_utils/techStack.tsx
+++ src/app/_utils/techStack.tsx
@@ -1,12 +1,44 @@
 import TechStack from "@/types/TechStack";
-import { FaHtml5 } from "react-icons/fa6";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaCss3, FaHtml5, FaReact } from "react-icons/fa6";
+import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
 
 const techStack: TechStack[] = [
 	{
 		id: '1',
 		name: 'HTML',
-		link: 'https://www.w3schools.com/html/html_intro.asp',
+		link: 'https://www.w3schools.com/html',
 		icon: <FaHtml5 />,
+	},
+	{
+		id: '2',
+		name: 'CSS',
+		link: 'https://www.w3schools.com/css',
+		icon: <FaCss3 />,
+	},
+	{
+		id: '3',
+		name: 'TS',
+		link: 'https://www.typescriptlang.org/docs/',
+		icon: <BiLogoTypescript />,
+	},
+	{
+		id: '4',
+		name: 'React',
+		link: 'https://react.dev',
+		icon: <FaReact />,
+	},
+	{
+		id: '5',
+		name: 'NextJS',
+		link: 'https://nextjs.org/docs',
+		icon: <RiNextjsLine />,
+	},
+	{
+		id: '6',
+		name: 'TailwindCSS',
+		link: 'https://tailwindcss.com',
+		icon: <RiTailwindCssFill />,
 	},
 ];
 
