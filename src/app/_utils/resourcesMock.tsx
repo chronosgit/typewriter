d32df13b3c4c0c537f6c1db33a789a74433577e3
chronosgit <warfacemeister@mail.ru>
2024-07-05 06:25:24 -0400

    feat: updated mock resources on home page

--- src/app/_utils/resourcesMock.tsx
+++ src/app/_utils/resourcesMock.tsx
@@ -1,5 +1,7 @@
 import Resource from '@/types/Resource';
+import { BsDiscord } from 'react-icons/bs';
 import { FiGithub } from 'react-icons/fi';
+import { LiaLinkedin } from 'react-icons/lia';
 
 const resourcesMock: Resource[] = [
 	{
@@ -9,6 +11,22 @@ const resourcesMock: Resource[] = [
 		linkUrl: 'https://github.com/chronosgit',
 		linkLabel: 'Check profile',
 		icon: <FiGithub />,
+	},
+	{
+		id: '2',
+		name: 'Linkedin',
+		descr: 'Let\' connect on Linkedin!',
+		linkUrl: 'https://kz.linkedin.com/in/nurken-kidirmaganbetov-338b412aa',
+		linkLabel: 'Connect',
+		icon: <LiaLinkedin />,
+	},
+	{
+		id: '3',
+		name: 'Discord',
+		descr: 'Add me on Discord!',
+		linkUrl: 'https://discordapp.com/users/chronosgit',
+		linkLabel: 'Find me',
+		icon: <BsDiscord />,
 	},
 ];
 
