46c124f4925d8744d75468b8748059fb61ad4146
chronosgit <warfacemeister@mail.ru>
2024-07-05 06:32:50 -0400

    feat: updated page flex on /

--- src/app/_components/layout/Layout.tsx
+++ src/app/_components/layout/Layout.tsx
@@ -21,7 +21,9 @@ const Layout: FC<Props> = ({ children }) => {
 		<div className="flex flex-col min-h-screen">
 			<Header />
 
-			{children}
+			<div className="flex-grow">
+				{children}
+			</div>
 
 			<Footer />
 		</div>
