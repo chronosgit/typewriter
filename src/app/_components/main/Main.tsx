55c20ad33d73859215ea7469b05fa2765af76225
chronosgit <warfacemeister@mail.ru>
2024-07-05 17:48:38 -0400

    feat: added init. version of main

---
+++ src/app/_components/main/Main.tsx
@@ -0,0 +1,18 @@
+const Main = () => {
+
+	return (
+		<section className="py-16">
+			<div className="max-w-screen-xl mx-auto">
+				<h3 className="mb-4 uppercase font-extralight tracking-widest text-center">
+					Typing speed test
+				</h3>
+
+				<h2 className="font-extrabold text-6xl text-center">
+					Test your typing skills
+				</h2>
+			</div>
+		</section>
+	);
+};
+
+export default Main;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Resources from '@/app/_components/resources/Resources';
 import TechStack from '@/app/_components/tech-stack/TechStack';
+import Main from '@/app/_components/main/Main';
 
 const App = () => {
 	return (
 		<div id="app">
+			<Main />
+
 			<Resources />
 
 			<TechStack />
