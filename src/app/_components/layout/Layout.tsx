aeb683a439fadba14e1c43622d0e921429b79e21
chronosgit <warfacemeister@mail.ru>
2024-07-20 13:15:08 +0500

    feat: updated theme await screen

--- src/app/_components/layout/Layout.tsx
+++ src/app/_components/layout/Layout.tsx
@@ -2,6 +2,7 @@
 
 import { FC, ReactNode, useContext } from 'react';
 import { usePathname } from 'next/navigation';
+import { ScaleLoader } from 'react-spinners';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import ThemeContext from '@/contexts/theme/ThemeContext';
@@ -25,9 +26,11 @@ const Layout: FC<Props> = ({ children }) => {
 		return <>{children}</>;
 	}
 
-	if (!themeCtx?.isPreferReady) {
+	if (themeCtx?.isPreferReady) {
 		return (
-			<div className="flex min-h-screen items-center justify-center bg-white">
+			<div className="flex flex-col min-h-screen items-center justify-center gap-4 bg-white">
+				<ScaleLoader />
+
 				<h1 className="text-black">Loading prefered color theme...</h1>
 			</div>
 		);
