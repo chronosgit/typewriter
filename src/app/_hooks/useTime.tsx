b61348bdc71af1453dd69fa06ed658cdb36ba2aa
chronosgit <warfacemeister@mail.ru>
2024-07-23 21:02:47 +0500

    feat: updated timer logic

--- src/app/_hooks/useTime.tsx
+++ src/app/_hooks/useTime.tsx
@@ -6,9 +6,36 @@ const useTime = (maxTime: number) => {
 	const [startedTyping, setStartedTyping] = useState(false);
 	const [remainingTime, setRemainingTime] = useState(maxTime);
 
-	const startTyping = () => setStartedTyping(true);
+	const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
 
-	return { startedTyping, startTyping, remainingTime };
+	const startTimer = () => {
+		if(timer) {
+			clearInterval(timer);
+		}
+
+		const newTimer = setInterval(() => {
+			console.log(remainingTime);
+
+			setRemainingTime(p => p - 1);
+		}, 1000);
+
+		setTimer(newTimer);
+	};
+
+	const stopTimer = () => {
+		if(timer) {
+			clearInterval(timer);
+			setTimer(null);
+		}
+	};
+
+	const startTyping = () => {
+		setStartedTyping(true);
+		
+		startTimer();
+	};
+
+	return { startedTyping, startTyping, remainingTime, startTimer, stopTimer };
 };
 
 export default useTime;
